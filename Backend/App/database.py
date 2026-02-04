8c5b6f7fe66c17735b4ead6d0e62257ba7988a66
FernandoAlvarezDEV <Ferjose0211@hotmail.com>
2026-02-04 13:00:31 -0400

    Modifacion de datos Sensibles

--- Backend/App/database.py
+++ Backend/App/database.py
@@ -1,8 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE = "mysql+mysqlconnector://root:Carlos591002*@localhost:3306/locompaautoimport"
+import os
+from dotenv import load_dotenv
+
+# Carga las variables del archivo .env
+load_dotenv()
+
+# Asigna las variables a constantes
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+URL_DATABASE = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{os.getenv('DB_PORT')}/{DB_NAME}"
 
 engine = create_engine(URL_DATABASE)
 
